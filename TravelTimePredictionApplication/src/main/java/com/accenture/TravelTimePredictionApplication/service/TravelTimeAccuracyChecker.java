8bdff342b44b924e86630601e8ad2b7895beae0e
Prigya <prigya.shukla@gmail.com>
2017-12-09 10:21:59 +0530

    added Time Accuracy checker

---
+++ TravelTimePredictionApplication/src/main/java/com/accenture/TravelTimePredictionApplication/service/TravelTimeAccuracyChecker.java
@@ -0,0 +1,70 @@
+package com.accenture.TravelTimePredictionApplication.service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class TravelTimeAccuracyChecker {
+	
+	//checking the accuracy of predicted data
+	
+	public static String SomeRealityCheck(String routeId) throws  Exception{
+		
+		//get route details
+		Route routeDetail = RouteDetails.getRoute(routeId);
+		System.out.println(routeId);
+		
+		//First getting the Traffic Data
+		String url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins="+routeDetail.getOrigin()+"&destinations="+routeDetail.getDestination()+"&departure_time=now&traffic_model=best_guess&mode=driving&key=AIzaSyBHz1QUGhHoP4ziV3SJRcmOAEEMukVGxtU";
+		
+		//https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=18.595416,73.770809&destinations=18.578769, 73.737213&departure_time=1552142340000&traffic_model=pessimistic&key=AIzaSyDxM4ZhGncwqAgNiKnSUO_CcxytBBpO1sE
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// optional default is GET
+		con.setRequestMethod("GET");
+		
+
+		//add request header
+		//con.setRequestProperty("User-Agent", "java call");
+		
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+		
+		//trying for json stuff here 
+		JSONObject msgObj = new JSONObject(response.toString()); // json object for traffic data 
+		
+		String durationInTraffic = msgObj.getJSONArray("rows")
+        .getJSONObject(0)
+        .getJSONArray ("elements")
+        .getJSONObject(0)
+        .getJSONObject("duration_in_traffic").getString("value");
+		
+		String originAdd = msgObj.getString("origin_addresses").toString().replace("[","").replace("]","").replace(",", "/");
+		String destinationAdd = msgObj.getString("destination_addresses").toString().replace("[","").replace("]","").replace(",", "/");
+		
+		System.out.println("origin: "+originAdd);
+		System.out.println("destination: "+destinationAdd);
+		
+		return durationInTraffic;
+	}
+
+}
