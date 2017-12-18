b21dc27f6f92327e43b39688d71969f7b4514932
Prigya <prigya.shukla@gmail.com>
2017-12-18 14:29:52 +0530

    using process builder instead of runtime.exec

--- TravelTimePredictionApplication/src/main/java/com/accenture/TravelTimePredictionApplication/service/TravelTimeRecommendor.java
+++ TravelTimePredictionApplication/src/main/java/com/accenture/TravelTimePredictionApplication/service/TravelTimeRecommendor.java
@@ -2,10 +2,13 @@ package com.accenture.TravelTimePredictionApplication.service;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 public class TravelTimeRecommendor {
 	
@@ -18,7 +21,7 @@ public class TravelTimeRecommendor {
     String[] cmdArray = new String[6];
 
     // first argument is the program we want to open
-    cmdArray[0] = "/usr/bin/Rscript";
+    cmdArray[0] = "/usr/lib/R/bin/Rscript";
     //cmdArray[0] = "C:\\Program Files\\R\\R-3.4.1\\bin\\Rscript";
 
     // second argument is a txt file we want to open with notepad
@@ -33,17 +36,33 @@ public class TravelTimeRecommendor {
     
 
 	
-	Runtime.getRuntime().exec(cmdArray);
+	/*Runtime.getRuntime().exec(new String[] {"/usr/bin/Rscript","/home/leanarch/spring_flow/R_models/loadtrafficModel.R","01","721","32","2"});
 	try {
 		//p.waitFor();
 		Thread.sleep(7000);
 	} catch (InterruptedException e1) {
 		// TODO Auto-generated catch block
 		e1.printStackTrace();
-	}
+	}*/
+    //trying to use Process builder instead
+    
+    Process process = new ProcessBuilder(cmdArray).start();
+    InputStream is = process.getInputStream();
+    InputStreamReader isr = new InputStreamReader(is);
+    BufferedReader br = new BufferedReader(isr);
+    String line;
+
+    System.out.printf("Output of running %s is:", 
+       Arrays.toString(cmdArray));
+
+    while ((line = br.readLine()) != null) {
+      System.out.println(line);
+    }
+
 	//Runtime.getRuntime().exec("C:\\Program Files\\R\\R-3.4.1\\bin\\Rscript C:\\Prigya\\RScript\\loadtrafficModel.R "+routeId+" "+weatherCode+" "+timezone+" "+weekDayN);
 	//Runtime.getRuntime().exec("/usr/lib/R/bin/Rscript /home/leanarch/spring_flow/R_models/loadtrafficModel.R "+routeId+" "+weatherCode+" "+timezone+" "+weekDayN);
 	System.out.println("done R");
+	System.out.println("version 9.3");
 	
 	
 	
